ecc7467e77bd5da7589746a5ac5f3c46bd816ba9
jyhan732 <jyhan@yahoo.com>
2021-03-28 18:44:47 -0400

    heroku deployment

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,15 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/fitnessApp", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+// mongoose.connect("mongodb://localhost/fitnessApp", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnessApp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 let workoutSeed = [
